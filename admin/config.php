660ad03b74ca293f0b85114bc1175b7fa3a23bf3
Saulius Okunevičius <saulius.okunevicius@onegoteam.com>
2011-10-18 17:16:35 +0300

    initial commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/admin/');
+define('HTTP_CATALOG', 'http://opencart/');
+define('HTTP_IMAGE', 'http://opencart/image/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/admin/');
+define('HTTPS_IMAGE', 'http://opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/local/apache2/htdocs/opencart/admin/');
+define('DIR_SYSTEM', '/opt/local/apache2/htdocs/opencart/system/');
+define('DIR_DATABASE', '/opt/local/apache2/htdocs/opencart/system/database/');
+define('DIR_LANGUAGE', '/opt/local/apache2/htdocs/opencart/admin/language/');
+define('DIR_TEMPLATE', '/opt/local/apache2/htdocs/opencart/admin/view/template/');
+define('DIR_CONFIG', '/opt/local/apache2/htdocs/opencart/system/config/');
+define('DIR_IMAGE', '/opt/local/apache2/htdocs/opencart/image/');
+define('DIR_CACHE', '/opt/local/apache2/htdocs/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/opt/local/apache2/htdocs/opencart/download/');
+define('DIR_LOGS', '/opt/local/apache2/htdocs/opencart/system/logs/');
+define('DIR_CATALOG', '/opt/local/apache2/htdocs/opencart/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart/');
+define('HTTP_IMAGE', 'http://opencart/image/');
+define('HTTP_ADMIN', 'http://opencart/admin/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart/');
+define('HTTPS_IMAGE', 'http://opencart/image/');
+
+// DIR
+define('DIR_APPLICATION', '/opt/local/apache2/htdocs/opencart/catalog/');
+define('DIR_SYSTEM', '/opt/local/apache2/htdocs/opencart/system/');
+define('DIR_DATABASE', '/opt/local/apache2/htdocs/opencart/system/database/');
+define('DIR_LANGUAGE', '/opt/local/apache2/htdocs/opencart/catalog/language/');
+define('DIR_TEMPLATE', '/opt/local/apache2/htdocs/opencart/catalog/view/theme/');
+define('DIR_CONFIG', '/opt/local/apache2/htdocs/opencart/system/config/');
+define('DIR_IMAGE', '/opt/local/apache2/htdocs/opencart/image/');
+define('DIR_CACHE', '/opt/local/apache2/htdocs/opencart/system/cache/');
+define('DIR_DOWNLOAD', '/opt/local/apache2/htdocs/opencart/download/');
+define('DIR_LOGS', '/opt/local/apache2/htdocs/opencart/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysql');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'opencart');
+define('DB_PASSWORD', 'opencart');
+define('DB_DATABASE', 'opencart');
+define('DB_PREFIX', '');
+?>
