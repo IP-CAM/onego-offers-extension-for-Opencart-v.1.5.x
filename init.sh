56b77f2e47e7caa617a4a0b632e67c96bc1314f1
Saulius Okunevičius <saulius.okunevicius@onegoteam.com>
2012-02-20 12:11:08 +0200

    init shell script

---
+++ init.sh
@@ -0,0 +1,12 @@
+mkdir cache
+mkdir download
+mkdir system/cache
+mkdir vqmod/vqcache
+chmod 0777 image
+chmod 0777 image/cache
+chmod 0777 system/cache
+chmod 0777 cache
+chmod 0777 download
+chmod 0777 config.php
+chmod 0777 admin/config.php
+chmod 0777 vqmod vqmod/vqcache
