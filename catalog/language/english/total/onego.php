68b6a23ba1192876cb43a534ac6e89386048c24d
Saulius Okunevičius <saulius.okunevicius@onegoteam.com>
2012-05-07 11:35:51 +0300

    Frontend texts updated

--- catalog/language/english/total/onego.php
+++ catalog/language/english/total/onego.php
@@ -1,63 +1,62 @@
 <?php
 // Heading 
-$_['heading_title'] = 'My Personal Benefits';
+$_['heading_title'] = 'My OneGo account';
 
 // Text
 $_['benefits_disabled'] = 'Your benefits withheld.';
 
 // Entry
-$_['button_onego_login']  = 'Apply my Benefits';
+$_['button_onego_login']  = 'Login';
 
 $_['onego_cart_discount'] = 'Your personal discount';
 $_['onego_cart_discount_percents'] = 'Your personal discount (%s%%)';
-$_['text_sub_total'] = 'Sub-Total with your benefits applied';
+$_['text_sub_total'] = 'Sub-Total with your offers applied';
 $_['prepaid_spent'] = 'Prepaid spent';
-$_['vgc_spent'] = 'Gift Card funds spent';
+$_['vgc_spent'] = 'Gift card funds spent';
 $_['funds_receivable'] = 'Your reward';
 $_['text_shipping_discount'] = 'Your shipping discount';
 
-$_['no_funds_available'] = 'You have no prepaid available.';
-$_['funds_prepaid'] = 'Use my prepaid: %s';
+$_['no_funds_available'] = 'You have no gift card balance available.';
+$_['funds_prepaid'] = 'Use my gift card balance: %s';
 
-$_['auth_widget_text_loading'] = 'Benefits by:';
-$_['auth_widget_text'] = 'Benefits by: {%NAME%}. {%LOGOUT|Not you?%}';
+$_['auth_widget_text_loading'] = 'Hello!';
+$_['auth_widget_text'] = 'Hello, {%NAME%}. {%LOGOUT|Not you?%}';
 
-$_['agree_email_expose'] = 'I agree to expose my e-mail to receive benefits for this and future purchases';
+$_['agree_email_expose'] = 'I agree to disclose my e-mail to OneGo for offers, rewards and gift cards from us for this and future purchases';
 
-$_['funds_received'] = 'You have received %s to your personal benefits account.';
-$_['funds_to_be_received'] = 'You will receive %s to your personal benefits account once we receive your payment and your order is confirmed.';
-$_['funds_receivable_descr'] = 'You will receive %s to your personal benefits account for future purchases if you agree to disclose your e-mail to OneGo.';
-$_['suggest_disclose'] = 'Do you agree that we disclose your e-mail address to OneGo to create your personal benefits account, to receive best offers from us in the future?';
+$_['funds_received'] = 'You have received %s into your gift card account.';
+$_['funds_to_be_received'] = 'You will receive %s into your gift card account once we receive your payment and your order is confirmed.';
+$_['funds_receivable_descr'] = 'You will receive %s into your gift card account for future purchases if you agree to disclose your e-mail to OneGo.';
+$_['suggest_disclose'] = 'Do you agree that we disclose your e-mail address to OneGo to create your account, to receive offers and rewards from us and use gift card balance in the future?';
 $_['button_agree_disclose'] = 'I agree';
-$_['title_claim_your_benefits'] = 'Claim your benefits';
-$_['title_benefits_claimed'] = 'You have claimed your OneGo benefits!';
-$_['anonymous_buyer_created'] = 'Your benefits account was created for you and your purchase was saved to your history for future reference.<br />'
-                                .'You can now proceed with completing your account registration using your e-mail address, or do it later at <a href="%s">onego.com</a> site.';
-$_['button_register_anonymous'] = 'Register your benefits account';
-$_['invite_to_login'] = 'Already have your benefits account?';
+$_['title_claim_your_benefits'] = 'Claim your offers and use gift card balance';
+$_['title_benefits_claimed'] = 'You have successfully claimed your offers!';
+$_['anonymous_buyer_created'] = 'Your OneGo account was created!<br />'
+    .'You can now proceed with completing your account registration using your e-mail address, or do it later at <a href="%s">onego.com</a> site.';
+$_['button_register_anonymous'] = 'Create OneGo account';
+$_['invite_to_login'] = 'Already have OneGo account?';
 $_['invite_to_use_vgc'] = 'Got a gift card?';
 $_['button_redeem'] = 'Redeem';
-$_['vgc_number'] = 'Gift Card Number';
+$_['vgc_number'] = 'Gift card number';
 $_['or'] = 'or';
 $_['button_try_again'] = 'Try again';
-$_['vgc_funds_redeemed'] = 'Virtual gift card funds';
-$_['vgc_remainder_plus_prepaid_available'] = 'Attention: you have not fully spent your virtual gift card funds.
+$_['vgc_funds_redeemed'] = 'Virtual gift card balance';
+$_['vgc_remainder_plus_prepaid_available'] = 'Attention: the remaining gift card balance may be lost.
     You will retain the remaining %s and also receive a %s reward if you agree to disclose
-    your e-mail address to create your personal benefits account.';
+    your e-mail address to OneGo to create your account.';
 $_['vgc_remainder_available'] = 'You will receive a %s reward if you agree to disclose
-    your e-mail address to create your personal benefits account.';
-$_['warning_cart_changed'] = 'Your benefits session has expired and was restarted, please re-check your order details.';
+    your e-mail address to OneGo to create your account.';
+$_['warning_cart_changed'] = 'Your OneGo session has expired and was restarted, please re-check your order details.';
 
 // Error
-$_['benefits_applied']  = 'Success: Your personal benefits applied.';
-$_['error_coupon']  = 'Warning: Coupon is either invalid, expired or reached it\'s usage limit!';
+$_['benefits_applied']  = 'Success: Your offers were applied.';
 
-$_['error_authorization_access_denied'] = 'Access to your benefits account denied';
-$_['error_authorization_temporarily_unavailable'] = 'Your benefits account is temporarily unavailable, please try again later.';
-$_['error_authorization_failed'] = 'We are sorry, benefits account server is not accessible right now. Please try again later.';
-$_['error_api_call_failed'] = 'We are sorry, benefits account server is not accessible right now. Please try again later.';
-$_['error_authentication_expired'] = 'Your benefits account session has expired, please login again.';
+$_['error_authorization_access_denied'] = 'Access to your OneGo account denied';
+$_['error_authorization_temporarily_unavailable'] = 'Your OneGo account is temporarily unavailable, please try again later.';
+$_['error_authorization_failed'] = 'We are sorry, OneGo server is not accessible right now. Please try again later.';
+$_['error_api_call_failed'] = 'We are sorry, OneGo server is not accessible right now. Please try again later.';
+$_['error_authentication_expired'] = 'Your OneGo account session has expired, please login again.';
 $_['error_bindnew_invalid_email'] = 'Error: your e-mail was not recognized as valid.';
-$_['error_bindnew_failed'] = 'Error: creating your personal benefits account failed, please try later.';
-$_['error_redeem_failed'] = 'Redeeming card failed, please try again later';
+$_['error_bindnew_failed'] = 'Error: creating your personal OneGo account failed, please try later.';
+$_['error_redeem_failed'] = 'Redeeming gift card failed, please try again later';
 $_['error_redeem_cardnumber_invalid'] = 'Gift card number is not valid';
